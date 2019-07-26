c0688f284a4c7a17aabeb9477e08d1ab713604ff
giulimarket <giuli.market@gmail.com>
2019-07-26 16:13:26 +0200

    Lezione 126

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,7 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by jt on 7/13/18.
  */
 public class Vet extends Person {
+	
+	private Set<Speciality> specialties;
+
+	public Set<Speciality> getSpecialties() {
+		return specialties;
+	}
+
+	public void setSpecialties(Set<Speciality> specialties) {
+		this.specialties = specialties;
+	}
 }
