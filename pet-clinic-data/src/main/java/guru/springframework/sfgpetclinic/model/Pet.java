49ae595b3e5b29df829c7ec94fcb68ff5c9f4410
giulimarket <giuli.market@gmail.com>
2019-09-27 10:33:13 +0200

    Lezione 143

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,14 +2,32 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  * Created by jt on 7/13/18.
  */
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
 
+	@Column(name= "name")
 	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "type_id")
     private PetType petType;
+	
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
     private Owner owner;
+	
+	@Column(name= "birth_date")
     private LocalDate birthDate;
 
     public PetType getPetType() {
